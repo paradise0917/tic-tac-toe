8ca947397374cfb0972666df6aa2793bb9f3ae65
Rita <paradise0917@gmail.com>
2019-11-02 16:11:46 +0800

    Updates

---
+++ precache-manifest.7e58c856d3d12b95dd6375c7608330db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2fca8926e3135b0c9d9d9334240933",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "407497a12865aaf2f87b",
+    "url": "/tic-tac-toe/static/css/main.5b891d96.chunk.css"
+  },
+  {
+    "revision": "6447b19b227df4ced1c9",
+    "url": "/tic-tac-toe/static/js/2.184221f6.chunk.js"
+  },
+  {
+    "revision": "407497a12865aaf2f87b",
+    "url": "/tic-tac-toe/static/js/main.62e29077.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  },
+  {
+    "revision": "1e3d489c4d62badfdb89228cae0180a3",
+    "url": "/tic-tac-toe/static/media/bak_2.1e3d489c.png"
+  }
+]);
